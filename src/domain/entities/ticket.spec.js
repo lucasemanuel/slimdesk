3082b0b9f1c73444d35c2549873ccd14dc802c3e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-13 10:17:05 -0300

    ensure return true if ticket is late

--- src/domain/entities/ticket.spec.js
+++ src/domain/entities/ticket.spec.js
@@ -1,6 +1,14 @@
 class TicketEntity {
   constructor ({ subject, body, createdAt, status }) {
+    this.slaHours = 24
     Object.assign(this, { subject, body, createdAt, status })
+  }
+
+  isLate () {
+    const currentAt = Date.now()
+    const millisecondsOfLate = 1000 * 60 * 60 * this.slaHours
+
+    return (this.createdAt.valueOf() + millisecondsOfLate) <= currentAt
   }
 }
 
@@ -20,6 +28,12 @@ const makeSut = () => {
   }
 }
 
+const makeDateWith24HoursLate = () => {
+  const date = new Date()
+  date.setDate(date.getDate() - 1)
+  return date
+}
+
 describe('Ticket Entity', () => {
   test('should contain subject, body, criation date and status', () => {
     const { sut, date } = makeSut()
@@ -28,8 +42,13 @@ describe('Ticket Entity', () => {
       subject: 'any_subject',
       body: 'any_body',
       createdAt: date,
-      status: 'any_status'
+      status: 'any_status',
+      slaHours: 24
     })
   })
-  test.todo('should return late status if ticket is late')
+  test('should return true if ticket is late', () => {
+    const { sut } = makeSut()
+    sut.createdAt = makeDateWith24HoursLate()
+    expect(sut.isLate()).toBeTruthy()
+  })
 })
