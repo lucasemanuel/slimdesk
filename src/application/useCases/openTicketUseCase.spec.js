d2a635a622b66b7c965722fc8c3e0d53ba5e390e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-16 12:29:42 -0300

    refactor validate params to execute OpenTicketUseCase

--- src/application/useCases/openTicketUseCase.spec.js
+++ src/application/useCases/openTicketUseCase.spec.js
@@ -1,18 +1,34 @@
+const { MissingParamError } = require('../../utils/errors')
+
 class OpenTicketUseCase {
-  async execute () {
-    throw new Error()
+  async execute ({ body, subject }) {
+    this.validate({ body, subject })
+  }
+
+  validate ({ body, subject }) {
+    if (!subject) {
+      throw new MissingParamError('subject')
+    } else if (!body) {
+      throw new MissingParamError('body')
+    }
   }
 }
 
 describe('Open Ticket Use Case', () => {
   test('should throw an error if is not provided subject', () => {
+    console.log(MissingParamError)
     const sut = new OpenTicketUseCase()
-    const promise = sut.execute({ body: '' })
-    expect(promise).rejects.toThrow()
+    const promise = sut.execute({ body: 'any_body' })
+    expect(promise).rejects.toThrow(new MissingParamError('subject'))
   })
   test('should throw an error if is not provided body', () => {
     const sut = new OpenTicketUseCase()
-    const promise = sut.execute({ subject: '' })
+    const promise = sut.execute({ subject: 'any_subject' })
+    expect(promise).rejects.toThrow(new MissingParamError('body'))
+  })
+  test.skip('should throw an error if TicketRepository is invalid', () => {
+    const sut = new OpenTicketUseCase()
+    const promise = sut.execute({ subject: 'any_subject', body: 'any_body' })
     expect(promise).rejects.toThrow()
   })
 })
