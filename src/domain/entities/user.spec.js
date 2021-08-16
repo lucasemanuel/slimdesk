0b9616a849b94f8258d1d4912c1b3a0f7e68f7e1
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-16 09:52:21 -0300

    ensure UserEntity contain id, name, email, phone, password and isActive status

---
+++ src/domain/entities/user.spec.js
@@ -0,0 +1,26 @@
+class UserEntity {
+  constructor ({ id, name, email, phone, password, isActive = true }) {
+    Object.assign(this, { id, name, email, phone, password, isActive })
+  }
+}
+
+describe('User Entity', () => {
+  test('should contain id, name, email, phone, password and isActive status', () => {
+    const sut = new UserEntity({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email',
+      phone: 'any_phone',
+      password: 'any_password'
+    })
+
+    expect(sut).toEqual(expect.objectContaining({
+      id: expect.any(String),
+      name: expect.any(String),
+      email: expect.any(String),
+      phone: expect.any(String),
+      password: expect.any(String),
+      isActive: expect.any(Boolean)
+    }))
+  })
+})
