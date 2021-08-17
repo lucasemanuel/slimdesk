3990ea4a134f9bb236d3270b4f6531115703934f
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-17 15:28:02 -0300

    move OpenTicketUseCase its to own file

--- src/application/useCases/openTicketUseCase.spec.js
+++ src/application/useCases/openTicketUseCase.spec.js
@@ -1,30 +1,5 @@
+const OpenTicketUseCase = require('./openTicketUseCase')
 const { MissingParamError } = require('../../utils/errors')
-
-class OpenTicketUseCase {
-  constructor ({ ticketFactory, distributorTicketsService, ticketRepository } = {}) {
-    Object.assign(this, {
-      ticketFactory,
-      distributorTicketsService,
-      ticketRepository
-    })
-  }
-
-  async execute ({ body, subject }) {
-    this.validate({ body, subject })
-    const ticket = this.ticketFactory.create({ body, subject })
-    const technician = this.distributorTicketsService.distribute()
-    this.ticketRepository.insert({ ticketEntity: ticket, userEntity: technician })
-    return ticket
-  }
-
-  validate ({ body, subject }) {
-    if (!subject) {
-      throw new MissingParamError('subject')
-    } else if (!body) {
-      throw new MissingParamError('body')
-    }
-  }
-}
 
 const makeTicketFactoryWithErrorSpy = () => {
   class TicketFactoryWithErrorSpy {
