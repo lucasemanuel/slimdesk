bf0b8b87024b0e57db829aef7baacc4f63169243
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-18 10:45:39 -0300

    move TicketStatus to its own file

---
+++ src/domain/constants/ticketStatus.js
@@ -0,0 +1,10 @@
+let TicketStatus = {
+  OPEN_STATUS: 'open_status',
+  DOING_STATUS: 'doing_status',
+  RESOLVED_STATUS: 'resolved_status',
+  LATE_STATUS: 'late_status'
+}
+
+TicketStatus = Object.freeze(TicketStatus)
+
+module.exports = TicketStatus
--- src/domain/constants/ticketStatus.spec.js
+++ src/domain/constants/ticketStatus.spec.js
@@ -1,11 +1,4 @@
-let TicketStatus = {
-  OPEN_STATUS: 'open_status',
-  DOING_STATUS: 'doing_status',
-  RESOLVED_STATUS: 'resolved_status',
-  LATE_STATUS: 'late_status'
-}
-
-TicketStatus = Object.freeze(TicketStatus)
+const TicketStatus = require('./ticketStatus')
 
 describe('Ticket Status', () => {
   test('should contain open status', () => {
