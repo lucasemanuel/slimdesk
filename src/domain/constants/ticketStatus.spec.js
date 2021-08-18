1320bc4b6ebef1f18eee64c205bf64b05ac2b760
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-18 10:44:47 -0300

    ensure that TicketStatus contain doing, resolved and late status

--- src/domain/constants/ticketStatus.spec.js
+++ src/domain/constants/ticketStatus.spec.js
@@ -1,5 +1,8 @@
 let TicketStatus = {
-  OPEN_STATUS: 'open_status'
+  OPEN_STATUS: 'open_status',
+  DOING_STATUS: 'doing_status',
+  RESOLVED_STATUS: 'resolved_status',
+  LATE_STATUS: 'late_status'
 }
 
 TicketStatus = Object.freeze(TicketStatus)
@@ -7,5 +10,14 @@ TicketStatus = Object.freeze(TicketStatus)
 describe('Ticket Status', () => {
   test('should contain open status', () => {
     expect(TicketStatus.OPEN_STATUS).toBe('open_status')
+  })
+  test('should contain doing status', () => {
+    expect(TicketStatus.DOING_STATUS).toBe('doing_status')
+  })
+  test('should contain resolved status', () => {
+    expect(TicketStatus.RESOLVED_STATUS).toBe('resolved_status')
+  })
+  test('should contain late status', () => {
+    expect(TicketStatus.LATE_STATUS).toBe('late_status')
   })
 })
